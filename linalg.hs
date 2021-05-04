c23a9b9ef72fb6f30587439bc5c4fa745445e3a7
Robert Long <longrob604@gmail.com>
2021-05-04 18:00:03 +0100

    Finished converting matrix of Int to matrix of Float

--- linalg.hs
+++ linalg.hs
@@ -12,6 +12,7 @@ m1, m2, m3 :: Matrix
 m1 = [[1.0,2.0], [3.0,4.0]]
 m2 = [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]
 m3 = [[1.0,2.0],[3.0,4.0],[5.0,6.0]]
+m4 = [[3,0,2],[2,0,-2],[0,1,1]]
 
 numRows :: Matrix -> Int
 numRows = length
@@ -79,5 +80,45 @@ cofactorMatrix m =
     n = length m
 
 inverse :: Matrix -> Matrix
-inverse m = transpose [ [ quot x (det m) | x <- row ] |
+inverse m = transpose [ [ x / (det m) | x <- row ] |
     row <- (cofactorMatrix m) ]
+
+elemMatrix :: Int -> Int -> Int -> Float -> Matrix
+ -- elemMatrix n i j v is the n-by-n elementary matrix
+ -- with v in the (i,j) place
+elemMatrix n i j v = [ [ entry row column | column <- [1..n] ] | row <- [1..n] ]
+   where
+   entry x y
+     | x == y = 1
+     | x == i && y == j = v
+     | otherwise = 0
+    
+idMatrix :: Int -> Matrix -- identity matrix
+idMatrix n = elemMatrix n 1 1 1
+
+eProduct :: Int -> [(Int, Int, Float)] -> Matrix
+-- eProduct n [(Int,Int,Float)] is the product of the elementary matrices
+eProduct n [ ] = idMatrix n
+eProduct n ((i,j,value):rest) = matrixProduct ( elemMatrix n i j value)
+  (eProduct n rest)
+
+minSize :: [(Int,Int,Float)] -> Int
+-- smallest size of matrix for which all elementary matrices are defined
+minSize list = maximum (concat [ [i,j] | (i,j,value) <- list ] )
+
+checkInverse :: [(Int,Int,Float)] -> String
+checkInverse list =
+  "\n M = " ++ (show m) ++ "\nInverse(M) = " ++ (show (inverse m)) ++
+  if matrixProduct m (inverse m) == idMatrix n then "\nOK.\n" else "\nError.\n"
+  where
+  m = eProduct n list
+  n = minSize list
+
+list1 :: [(Int,Int,Float)]
+list1 = [(1,2,1.0), (1,3,-1.0), (1,2,1.0), (3,2,-2.0), (3,1,-3.0)]
+list2 :: [(Int,Int,Float)]
+list2 = [(1,2,4.0), (4,2,-1.0), (4,1,-2.0), (4,1,1.0), (1,3,-3.0), (2,3,2.0),
+  (1,2,2.0), (1,4,-3.0), (1,3,-1.0), (3,2,-1.0), (3,1,-1.0)]
+  
+test :: IO()
+test = putStr (( checkInverse list1 ) ++ ( checkInverse list2 ) )
